08ad54a5a0f90234107b79678d454de71c0716e4
Jared Vacanti <jaredvacanti@gmail.com>
2020-07-04 19:57:52 -0500

    adding test suites with dummy private ecc and rsa keys; test if generation matches

--- secrets_to_paper/generate/rsa.py
+++ secrets_to_paper/generate/rsa.py
@@ -1,4 +1,6 @@
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.serialization import (
     PublicFormat,
     Encoding,
