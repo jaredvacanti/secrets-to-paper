c3b5b1344801326dc59f4fec2433fb63766a0536
Jared Vacanti <jaredvacanti@gmail.com>
2020-07-05 11:32:11 -0500

    parsing pdfs by filepath in order to decode QR codes

--- secrets_to_paper/parse.py
+++ secrets_to_paper/parse.py
@@ -1,51 +1,16 @@
 import subprocess
 import base64
+from pdf2image import convert_from_path, convert_from_bytes
+from pyzbar.pyzbar import decode
 
 
 def pdf_to_secret(pdf_file):
-    return None
+    pages = convert_from_path(pdf_file)
 
+    print(pages)
+    for page in pages:
 
-def import_from_b64(pubkey_path, private_bytes):
-    """
-    Imports the private GPG key to the gpg key ring.
-    """
+        codes = decode(page)
+        for code in codes:
+            print(code.data.decode("ascii"))
 
-    paperkey = subprocess.Popen(
-        args=["paperkey", "--pubring", pubkey_path],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-    )
-    (paperkey_stdout, _) = paperkey.communicate(private_bytes)
-    gpg = subprocess.Popen(
-        args=["gpg", "--import"],
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    return gpg.communicate(paperkey_stdout)
-
-
-def read_chunks_png(in_filenames):
-    """
-    Reads base64 encoded private key data from many PNG QR codes.
-    """
-
-    base64str = b""
-    for in_filename in in_filenames:
-        chunk = subprocess.check_output(["zbarimg", "--raw", in_filename])
-        base64str += chunk
-    return base64str
-
-
-def read_chunks_b64(in_filenames):
-    """
-    Reads base64 encoded private key data from many files.
-    """
-
-    base64str = b""
-    for in_filename in in_filenames:
-        with open(in_filename, "rb") as in_file:
-            chunk = in_file.read()
-            base64str += chunk
-    return base64str
