3f881eece3ba42209cc93d596dbab06821cdae20
Jared Vacanti <jaredvacanti@gmail.com>
2020-07-08 23:05:06 -0500

    creating ecc curves from named curves and secret number"

--- secrets_to_paper/generate/ecc.py
+++ secrets_to_paper/generate/ecc.py
@@ -1,4 +1,5 @@
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.serialization import (
     PublicFormat,
     Encoding,
@@ -9,29 +10,43 @@ from cryptography.hazmat.primitives.serialization import (
 from cryptography.hazmat.primitives.asymmetric.ec import (
     EllipticCurvePrivateNumbers,
     EllipticCurvePublicNumbers,
+    EllipticCurve,
+    ECDSA,
     SECP256R1,
+    EllipticCurvePrivateKey,
     EllipticCurvePublicKey,
     EllipticCurvePrivateKeyWithSerialization,
     derive_private_key,
 )
 
 
-def generate_ecc_key(secret_number, public_number):
+def generate_ecc_key(
+    secret_number=None, private_key: EllipticCurvePrivateKey = None, curve="secp256r1"
+):
 
-    secret_int = int(secret_number, 16)
+    if curve == "secp256r1":
+        curve = SECP256R1()
+    else:
+        raise NotImplementedError("Curve type not implemented.")
 
-    public_key = EllipticCurvePublicKey.from_encoded_point(
-        SECP256R1(), bytes.fromhex(public_number)
-    )
-    pubkey = public_key.public_bytes(
+    if secret_number:
+        secret_int = int(secret_number, 16)
+        priv_key = derive_private_key(secret_int, curve, default_backend())
+    elif private_key:
+        priv_key = private_key
+
+    # Make private and public keys from the private value + curve
+    pub_key = priv_key.public_key()
+
+    pubkey = pub_key.public_bytes(
         Encoding.PEM, PublicFormat.SubjectPublicKeyInfo
     ).decode("ascii")
 
-    print(pubkey)
-
-    derived_key = derive_private_key(secret_int, SECP256R1(), default_backend())
-    private_key = derived_key.private_bytes(
+    # derived_key = derive_private_key(secret_int, SECP256R1(), default_backend())
+    private_key = priv_key.private_bytes(
         Encoding.PEM, PrivateFormat.TraditionalOpenSSL, NoEncryption()
     ).decode("ascii")
 
+    print(pubkey)
     print(private_key)
+
